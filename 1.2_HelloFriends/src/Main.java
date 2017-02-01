7e28bc9bf17eb28b621d1737133cc55b265a7492
Jamal Doorson <JamalDoorson@live.nl>
2017-02-01 23:02:20 +0100

    1.2 af

--- 1.2_HelloFriends/src/Main.java
+++ 1.2_HelloFriends/src/Main.java
@@ -1,20 +1,60 @@
+import java.io.*;
 import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
+
+		String name = askName(input);
+		printFile(input);
+		printFile(input, name);
+		System.out.println(new StringBuffer(name).reverse().toString());
+		
+		input.close();
+	}
+
+	public static String askName(Scanner input) {
 		System.out.println("What's your name?");
 		String name = input.nextLine();
-		
-		switch(name)
-		{
+
+		switch (name) {
 		case "Nelleke":
 			System.out.println("een speciale begroeting :)");
 			break;
 		default:
 			System.out.println("Hello, " + name + "!");
 		}
+		
+		return name;
 	}
 
+	public static void printFile(Scanner input) {
+		input = null;
+		
+		try {
+			input = new Scanner(new File("friends.txt"));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+
+		while (input.hasNextLine()) {
+			System.out.println(input.nextLine());
+		}
+	}
+	
+	public static void printFile(Scanner input, String name)
+	{
+		input = null;
+		
+		try {
+			input = new Scanner(new File(name + ".txt"));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+
+		while (input.hasNextLine()) {
+			System.out.println(input.nextLine());
+		}
+	}
 }
