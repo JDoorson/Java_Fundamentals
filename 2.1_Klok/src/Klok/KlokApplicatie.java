15bea3144f8dc1d5d9d416893abd627f22da069c
JDoorson <JDoorson@github.com>
2017-02-09 13:16:41 +0100

    PulseController fix

--- 2.1_Klok/src/Klok/KlokApplicatie.java
+++ 2.1_Klok/src/Klok/KlokApplicatie.java
@@ -1,17 +1,31 @@
 package Klok;
 
 import javax.swing.*;
+import java.util.Scanner;
+
 /**
  * Created by Jamal on 03/02/2017.
  */
 public class KlokApplicatie {
-
-    public static void main(String[] args)
+    public KlokApplicatie()
     {
         KlokModel model = new KlokModel(12, 0);
         KlokConsoleView view = new KlokConsoleView(model);
 
         Timer pulse = new Timer(1000, new PulseController(model));
         pulse.start();
+
+        waitForEnter();
+    }
+
+    public static void main(String[] args)
+    {
+        new KlokApplicatie();
+    }
+
+    private void waitForEnter()
+    {
+        new Scanner( System.in ).nextLine();
+        System.exit( 0 );
     }
 }
